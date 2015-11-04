00d0f34fa69456335e2502bf7d3007f75fcd5886
Cristian <cristian.canosa.perez@udc.es>
2015-11-04 19:48:33 +0100

    Comentarios de Contenido.java

--- practica-vvs/src/contenido/Contenido.java
+++ practica-vvs/src/contenido/Contenido.java
@@ -3,10 +3,21 @@ package contenido;
 import java.util.List;
 
 public interface Contenido {
+	
+	//Se piden tres implementaciones: una para canciones, otra para anuncios, y otra para emisoras
+	//Los anuncios duran siempre 5 segundos
+	//Los anuncions tienen siempre como título 'PUBLICIDAD'
+	
 	public String obtenerTitulo();
 	public Integer obtenerDuracion();
 	public List<Contenido> obtenerListaReproduccion();
+	
+	//Buscar realiza comparaciones usando el título
 	public List<Contenido> buscar(String subcadena);
+	
+	//En canciones y anuncios agregar no tiene efecto
 	public void agregar(Contenido contenido, Contenido predecesor);
+	
+	//En canciones y anuncios eliminar no tiene efecto
 	public void eliminar(Contenido contenido);
 }
