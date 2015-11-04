e6f960d1e631e8f77647870aec6226e6b24bc5e4
Xoán Antelo Castro <xoan.antelo@udc.es>
2015-11-04 23:38:35 +0100

    Test de probas para o travis

---
+++ test/pruebas/TravisTest.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TravisTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
