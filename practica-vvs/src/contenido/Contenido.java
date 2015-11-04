13fc0799b70912f6b3883cd3bb843485224e82c9
Francisco Pais Fondo <francisco.pais1@udc.es>
2015-11-04 19:30:28 +0100

    Interfaz Contenido

--- practica-vvs/src/contenido/Contenido.java
+++ practica-vvs/src/contenido/Contenido.java
@@ -1,5 +1,12 @@
 package contenido;
 
-public interface Contenido {
+import java.util.List;
 
+public interface Contenido {
+	public String obtenerTitulo();
+	public Integer obtenerDuracion();
+	public List<Contenido> obtenerListaReproduccion();
+	public List<Contenido> buscar(String subcadena);
+	public void agregar(Contenido contenido, Contenido predecesor);
+	public void eliminar(Contenido contenido);
 }
