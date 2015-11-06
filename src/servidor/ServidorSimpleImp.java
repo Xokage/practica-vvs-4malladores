e64f729b260a67a9d12f3659b0ef0f805112aacb
Cristian <cristian.canosa.perez@udc.es>
2015-11-06 15:06:44 +0100

    Cambios no método buscar de ServidorSimpleImp

--- src/servidor/ServidorSimpleImp.java
+++ src/servidor/ServidorSimpleImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import util.Pair;
+import contenido.Anuncio;
 import contenido.Contenido;
 
 public class ServidorSimpleImp implements Servidor {
@@ -54,14 +55,9 @@ public class ServidorSimpleImp implements Servidor {
 
 	@Override
 	public String alta() {
-		boolean existe=true;
-		Pair par = null;
-		while (existe){
-			existe=false;
-			par = getToken(10);
-			for (Pair parColeccion : tokenValido ) {
-				if (parColeccion.getLeft().toString().equals(par.getLeft().toString())) existe=true;
-			}
+		Pair par = getToken(10);
+		while (tokenValido.contains(par)){
+			par=getToken(10);
 		}
 		tokenValido.add(par);
 		return par.getLeft().toString();
@@ -95,36 +91,42 @@ public class ServidorSimpleImp implements Servidor {
 	public List<Contenido> buscar(String subcadena, String token) {
 		List<Contenido> resultado = new ArrayList<>();
 		Pair<String, Integer> user = null;
-		
-		for(Pair<String, Integer> tmpUser : this.tokenValido){
-			if(tmpUser.getLeft().equals(token))
+
+		for (Pair<String, Integer> tmpUser : this.tokenValido) {
+			if (tmpUser.getLeft().equals(token))
 				user = tmpUser;
 		}
-		if (user!=null)	this.tokenValido.remove(user); 
-												//Quitamos o usuario da lista de tokens validos
-												//mentres está pedindo contido.
-		int contadorDeAnuncios = 3;
-		for(Contenido elemento : this.contenidoList){
-			if(user.getRight()<=0){
-				baja(user.getLeft()); //Se o token non 
-			 						  //ten mais usos eliminamolo.
-				user = null;
-			}
-			
-			if(user == null){		//Se non hai token valido
-				if(contadorDeAnuncios>=3){
-					contadorDeAnuncios = 0;
-					//TODO meter un anuncio.
-				} else contadorDeAnuncios++;
-			}
-			
-			if(elemento.obtenerTitulo().contains(subcadena)){
-				resultado.add(elemento);
-				user = new Pair<String, Integer>(user.getLeft(),user.getRight() - 1);
+		if (user != null)
+			this.tokenValido.remove(user);
+		// Quitamos o usuario da lista de tokens validos
+		// mentres estÃ¡ pedindo contido.
+		int contidosVisualizados = 0;
+		for (Contenido elemento : this.contenidoList) {
+
+			if (elemento.obtenerTitulo().contains(subcadena)) {
+
+				if (user.getRight() <= 0) { // usuario queda sen intentos,
+											// comezan os anuncios
+					if (contidosVisualizados == 0)
+						resultado.add(new Anuncio()); // Comeza cun anuncio
+					contidosVisualizados++;
+					resultado.add(elemento); // Añadimos elemento que contén a
+												// subcadena
+					if (contidosVisualizados % 3 == 0)
+						resultado.add(new Anuncio()); // Un anuncio cada 3
+														// contidos
+				} else {
+					resultado.add(elemento); // Añadimos elemento que contén a
+												// subcadena
+					user = new Pair<String, Integer>(user.getLeft(),
+							user.getRight() - 1);// Usuario gasta un intento
+				}
 			}
 		}
-		
+
+		if (user.getRight() > 0) // Token non é vacío, por tanto mantense.
+			tokenValido.add(user);
+
 		return resultado;
 	}
-
 }
