e85335cd52e79e6128d6a7922ec1c7f986b505bb
Francisco Pais Fondo <francisco.pais1@udc.es>
2015-11-06 15:56:29 +0100

    Creado test de Anuncio

---
+++ src/test/TestAnuncio.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import contenido.Anuncio;
+import contenido.Contenido;
+import junit.framework.TestCase;
+
+public class TestAnuncio extends TestCase {
+		
+		Anuncio anuncio = new Anuncio();
+
+		@Test
+		public void obtenerTituloTest() {
+			assertTrue(anuncio.obtenerTitulo().equals("PUBLICIDAD"));
+		}
+		
+		@Test
+		public void obtenerDuracionTest() {
+			assertTrue(Integer.compare(anuncio.obtenerDuracion(),5)==0);
+		}
+		
+		@Test
+		public void obtenerListaReproduccionTest() {
+			List<Contenido> listaReproduccionIdeal = new ArrayList();
+			listaReproduccionIdeal.add(anuncio);
+			assertTrue(anuncio.obtenerListaReproduccion().equals(listaReproduccionIdeal));
+		}
+		
+		@Test
+		public void buscarTest() {
+			List<Contenido> listaReproduccionIdeal = new ArrayList();
+			listaReproduccionIdeal.add(anuncio);
+			assertTrue(anuncio.buscar("PUBLICIDAD").equals(listaReproduccionIdeal));
+			listaReproduccionIdeal = new ArrayList();
+			assertTrue(anuncio.buscar("OTRA COSA").equals(listaReproduccionIdeal));
+		}
+}
